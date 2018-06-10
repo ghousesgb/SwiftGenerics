8e17221018b09eb26873ed0b68856153221dbd44
Ghouse Shaik <ghousebashashaik@apples-macbook-pro.local>
2018-06-10 22:21:37 +0530

    Initial Commit

---
+++ GenericFunctions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GenericFunctions
+//
+//  Created by Ghouse Basha Shaik on 10/06/18.
+//  Copyright © 2018 Ghouse Basha Shaik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
